26fa211dbe962214ca089deaadd9d0bad7598e20
Shoham Galili <131040585+ShohamGalili@users.noreply.github.com>
2024-08-13 16:11:09 +0300

    Add files via upload

---
+++ Advanced Assembly: Real-Time Clock TSR and Interactive Text Display/clockTSR.asm
@@ -0,0 +1,145 @@
+;Hadas Yosefzada 213486764
+;Shoham Galili 208010785
+
+.model tiny
+
+.code
+	;public ISR_New_1C
+	public finish_tsr
+	public HERE
+	extern print_main:near
+	ms dw 00d ;ms of timer
+	seconds dw 00d ;seconds of timer
+	minutes dw 00d ;minutes of timer
+	backcolor db 3d ;turkiz color
+	recent_ah db 0d
+	org 100h ;making space in the memory for PSP
+
+HERE:
+	jmp IVT_REPLACE
+
+	
+;*********************
+;*********************
+;ISR_New_1C
+;Outputs: This function change int 1C
+;*********************	
+ISR_New_1C proc far uses ax es dx bx cx
+
+	mov ax, 0B800h
+	mov es, ax        ;Set ax to Screen
+	
+	;print minutes on screen
+		mov ax , minutes
+		mov bh , 10d
+		div bh ;al=(int)ax/bh , ah=ax mod bh
+		add ah , 30h ;convert to ascii
+		add al , 30h ;convert to ascii
+		mov recent_ah , ah
+		mov ah , backcolor
+		mov es:[0h] , ax
+		mov al , recent_ah
+		mov es:[2h] , ax
+		
+		mov al ,':' ;ascii to print
+		mov es:[4h] , ax
+		
+		;print seconds on screen
+		mov ax , seconds
+		mov bh , 10d
+		div bh ;al=(int)ax/bh , ah=ax mod bh
+		add ah , 30h ;convert to ascii
+		add al , 30h ;convert to ascii
+		mov recent_ah , ah
+		mov ah , backcolor
+		mov es:[6h] , ax
+		mov al , recent_ah
+		mov es:[8h] , ax
+		
+		mov al ,':' ;ascii to print
+		mov es:[10d] , ax
+		
+		
+		;print ms on screen
+		mov ax , ms
+		mov bh , 10d
+		div bh ;al=(int)ax/bh , ah=ax mod bh
+		add ah , 30h ;convert to ascii
+		mov bl , ah ;the Unity digit
+		mov ah ,0 ;ax=al
+		div bh ;al=(int)ax/bh , ah=ax mod bh
+		add ah , 30h ;convert to ascii
+		add al , 30h ;convert to ascii
+		mov recent_ah , ah
+		mov ah , backcolor
+		mov es:[12d] , ax ;hundreds digit
+		mov al , recent_ah
+		mov es:[14d] , ax ;tens digit
+		mov bh , backcolor
+		mov es:[16d] , bx ;the Unity digit
+		
+	;calc the timer
+	
+	cmp ms , 990d ;990 ms is a mul of 55ms and almost a sec
+	jnz inc_ms
+	
+	;if has past one sec
+	mov ms , 0d ;reset ms
+	cmp seconds , 59d ;60 sec is one minute
+	jnz inc_sec
+	
+	;if has past one minute
+	mov seconds , 0d;reset sec
+	inc minutes ;min++
+	jmp prev_isr ;we dont want to inc sec
+	
+	inc_sec:
+	inc seconds ;sec++
+	cmp backcolor ,3d
+	jnz change_to_turkiz
+	mov backcolor ,5d ;magenta color
+	jmp prev_isr
+	change_to_turkiz:
+	mov backcolor ,3d
+	jmp prev_isr ;we dont want to inc ms
+	
+	inc_ms:
+	add ms, 55d ;the interrupt happens every 55ms
+	
+	prev_isr:
+		int 80h ;use the old interrupt
+		iret
+	
+ISR_New_1C endp
+	
+	End_TSR:
+	
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+;~~~~~~~~~~~~~~~~MAIN~~~~~~~~~~~~~~~~~
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	IVT_REPLACE:
+	; IVT is location is '0000' address of RAM
+	mov ax,0h
+	mov es,ax
+	
+	cli
+	;moving Int1C into IVT[0a0h]
+	mov ax,es:[1Ch*4] ;copying old ISR1C IP to free vector
+	mov es:[80h*4],ax
+	mov ax,es:[1Ch*4+2] ;copying old ISR1C CS to free vector
+	mov es:[80h*4+2],ax
+	;moving ISR_New_1C into IVT[1C]
+	mov ax,offset ISR_New_1C ;copying IP of ISR_New to IVT[1C]
+	mov es:[1Ch*4],ax
+	mov ax,cs ;copying CS of ISR_New to IVT[1C]
+	mov es:[1Ch*4+2],ax
+	sti
+	
+	jmp print_main
+	
+	finish_tsr:
+	;exit the program and saving ISR_New_1C in the TSR
+	mov dx, offset End_TSR
+	int 27h
+	
+end 
---
+++ Advanced Assembly: Real-Time Clock TSR and Interactive Text Display/print.asm
@@ -0,0 +1,189 @@
+;Hadas Yosefzada 213486764
+;Shoham Galili 208010785
+
+.model small
+.stack 100h
+.data
+	out_msg db '"It',"'",'s hardware that makes a machine fast. It',"'",'s software that makes a fast machine slow." - Craig Bruce','$'
+	count_3_times db 0
+	row db 12
+	column db 0
+	backgroundColor db 1Fh
+	sec_to_wait dw 23d
+	flag_continue_or_stop_writing db 0 ;the flag is 0 if im in continue and 1 if i stoped
+.code
+	public print_main
+	;extern ISR_New_1C:far
+	extern finish_tsr:near
+	extern HERE:near
+;Print Black background on screen
+	Black_Screen PROC near uses es ax cx si
+	
+		mov ax, 0B800h
+		mov es, ax        ;Set ax to Screen
+		mov si, 0h
+		mov ax,' '        ;Print ' ' to screen 
+		mov cx, 1280h     ;Set cx to Screen Size
+		
+		ScreenLoop:	
+			mov es:[si],ax
+			sub cx, 2h
+			add si, 2h
+			cmp cx, 0 
+		jnz ScreenLoop
+		
+	ret
+	Black_Screen ENDP
+	
+	;*********************
+	
+;Wait one second
+	Delay_one_sec PROC near uses ax cx 
+	
+		if_key_was_pressed:
+		in al,64h
+		test al, 00000001b ;Check Lsb- if LSB ==1 --> Key was pressed if LSB ==0 --> Key wasn't pressed
+		jz delay
+			
+		in al,60h ;Read Scan Code from keyboard port
+		
+		Check_plus: 
+		cmp al, 31d ;Check if s has pressed - speed it up
+		jnz check_if_minus
+		shr sec_to_wait ,1 ;sec_to_wait = sec_to_wait / 2
+			
+		check_if_minus:
+		cmp al, 32d ;Check if d has pressed - delay it
+		jnz check_if_p
+		shl sec_to_wait ,1 ;sec_to_wait = sec_to_wait * 2
+		
+		check_if_p:
+		cmp al, 25d ;Check if p has pressed - stop or continue writing
+		jnz delay
+		cmp flag_continue_or_stop_writing , 0 ;check if the flag is zero
+		jz change_flag_to_1 
+		mov flag_continue_or_stop_writing , 0 ;if flag == 1 change flag to 0
+		jmp delay
+		change_flag_to_1:
+		mov flag_continue_or_stop_writing , 1 ;if flag == 0 change flag to 1
+		
+		delay:
+		mov ax,sec_to_wait ;sec_to_wait times for the loop of 100ms
+		outLoop:
+			xor cx,cx ;cx=0
+		wait100msec:
+			nop
+		loop wait100msec
+		
+		dec ax ;ax=ax-1.
+		jnz outLoop
+		
+	;if past a sec:	
+	end_Delay_one_sec:	
+	ret
+	Delay_one_sec ENDP
+	
+;*********************
+
+;*********************
+;********main*********
+;*********************
+	.startup
+	call Black_Screen
+	jmp HERE
+	
+	print_main:
+	
+	;resat all registers
+	xor ax,ax
+	xor cx,cx
+	xor si,si
+	;xor es,es
+	xor bx,bx
+	xor dx,dx
+	
+	
+	
+	;mask interrupts from keyboard
+	cli
+	in al, 21h
+	or al, 02h
+	out 21h, al
+	sti
+	
+	mov cx,102d ; the length of the full sentence
+	mov si,0 ;reset counter on the string
+	
+	;;call Black_Screen
+	
+	;SET CURSOR POSITION
+	mov ah,02h ;use service 2 of int 10h
+	mov dh,12d ;row number 12
+	mov dl,0d ;coulomn number 0
+	int 10h ;now the cursor position is in the center
+	
+	;now the loop starts:
+	print_sentence:
+	cmp flag_continue_or_stop_writing , 1 ;check if the flag is 1
+	jz if_flag_1 ;we dont want to change the backgrount if we are on stop
+	cmp count_3_times,3 ;if past 3 times
+	jnz continue
+	cmp backgroundColor, 7Fh ; the limit
+	jnz continue_1
+	mov backgroundColor,0Fh
+	continue_1:
+	mov count_3_times,0 ;count_3_times=0
+	add backgroundColor , 10h
+	
+	continue:
+	;PRINT OUTPUT with color
+	push cx
+	mov cx,1
+	mov al, out_msg[si] ;one character
+	mov bl , backgroundColor ; background color
+	mov ah , 09h ;use service 09h of int 10h
+	int 10h
+	pop cx
+	;cmp flag_continue_or_stop_writing , 0 ;check if flag = 0 --> continue print_sentence
+	;jnz CURSOR_POSITION
+	;if flag = 0 we want to continue the print
+	inc si ;si++
+	inc column ;column++
+
+	CURSOR_POSITION:
+	;SET CURSOR POSITION
+	mov ah,02h ;use service 2 of int 10h
+	mov dh,row ;row
+	mov dl,column ;coulomn
+	int 10h ;now the cursor position is in the center
+	
+	inc count_3_times ;count_3_times++
+	jmp end_loop ; if flag = 0 we dont want to inc cx
+	
+	if_flag_1:
+	inc cx ;save every loop the time tat i need for the whole sentence
+	
+	end_loop:
+	call Delay_one_sec
+	
+	loop print_sentence
+	
+	;out of the loop:
+	add row,2
+	;SET CURSOR POSITION
+	mov ah,02h ;use service 2 of int 10h
+	mov dh,row ;row
+	mov dl,0 ;coulomn
+	int 10h ;now the cursor position is in the center
+	
+	;unmask keyboard
+	cli
+	mov al,0
+	out 21h,al
+	sti
+	
+	jmp finish_tsr
+	
+.exit
+
+end
